c1c4994609dc78885e7faee3aa0e412579e6e809
madefafa <madefafa@163.com>
2021-06-16 20:56:49 +0800

    Add Second APP

---
+++ code/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好,世界!");
+            Console.ReadKey();
+        }
+    }
+}
